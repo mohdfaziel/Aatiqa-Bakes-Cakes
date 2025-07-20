ed42b63a8f88f7d5e6f7a93cf804a4d62d51fe16
Mohd Faziel <mohdfazel969@gmail.com>
2025-07-21 01:11:45 +0530

    modified portfolio link

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer()
 {
     return (
         <div className="w-full bg-dark1 h-[6rem] flex flex-col gap-2 justify-center items-center">
-            <p className="text-unactive text-sm">Design & Built by <a href="https://www.faziel.me/" target="_blank"><span className="text-active ">Mohd Faziel</span></a> © 2024</p>
+            <p className="text-unactive text-sm">Design & Built by <a href="https://faziel.vercel.app/" target="_blank"><span className="text-active ">Mohd Faziel</span></a> © 2024</p>
             <Social/>
         </div>
     )
